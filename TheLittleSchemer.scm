9dcf9b423e1d5bc2579723be8cac9fefca961fc8
MBoswinkel <mdboswinkel@outlook.com>
2025-02-11 14:15:37 +0100

    Code for chapter 6 added

--- TheLittleSchemer.scm
+++ TheLittleSchemer.scm
@@ -290,3 +290,47 @@
       ((null? l) (quote ()))
       ((equal? (car l) s) (cdr l))
       (else (cons (car l) (rember s (cdr l)))))))
+
+(define numbered?
+  (lambda (aexp)
+    (cond
+      ((atom? aexp) (number? aexp))
+      (else (and (numbered? (car aexp)) (numbered? (car (cdr (cdr aexp)))))))))
+
+(define 2nd-sub-exp
+  (lambda (aexp)
+    (car (cdr (cdr aexp)))))
+
+(define value
+  (lambda (nexp)
+    (cond
+      ((atom? nexp) nexp)
+      ((eq? (operator nexp) (quote +)) (o+ (value (1st-sub-exp nexp)) (value (2nd-sub-exp nexp))))
+      ((eq? (operator nexp) (quote *)) (* (value (1st-sub-exp nexp)) (value (2nd-sub-exp nexp))))
+      (else (expt (value (1st-sub-exp nexp)) (value (2nd-sub-exp nexp)))))))
+
+(define 1st-sub-exp
+  (lambda (aexp)
+    (car aexp)))
+
+(define operator
+  (lambda (aexp)
+    (car (cdr aexp))))
+
+(define sero?
+  (lambda (n)
+    ((null? n))))
+
+(define edd1
+  (lambda (n)
+    (cons (quote ()) n)))
+
+(define zub1
+  (lambda (n)
+    (cdr n)))
+
+(define p+
+  (lambda (n m)
+    (cond
+      ((sero? m) n)
+      (else (edd1 (p+ n (zub1 m)))))))

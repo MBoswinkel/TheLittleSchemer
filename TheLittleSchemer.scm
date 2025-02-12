940f9c7b9b00e790829b2dc4b885fd3653eb0412
MBoswinkel <mdboswinkel@outlook.com>
2025-02-12 17:02:17 +0100

    Code for chapter 7 added

--- TheLittleSchemer.scm
+++ TheLittleSchemer.scm
@@ -11,12 +11,6 @@
       ((atom? (car l)) (lat? (cdr l)))
       (else #f))))
 
-(define member?
-  (lambda (a lat)
-    (cond
-      ((null? lat) #f)
-      (else (or (eq? (car lat) a) (member? a (cdr lat)))))))
-
 (define firsts
   (lambda (l)
     (cond
@@ -54,14 +48,6 @@
       (else (cond
               ((or (eq? (car lat) o1) (eq? (car lat) o2)) (cons new (cdr lat)))
               (else (cons (car lat) (subst2 new o1 o2 (cdr lat)))))))))
-
-(define multirember
-  (lambda (a lat)
-    (cond
-      ((null? lat) (quote ()))
-      (else (cond
-              ((eq? (car lat) a) (multirember a (cdr lat)))
-              (else (cons (car lat) (multirember a (cdr lat)))))))))
 
 (define multiinsertR
   (lambda (new old lat)
@@ -334,3 +320,125 @@
     (cond
       ((sero? m) n)
       (else (edd1 (p+ n (zub1 m)))))))
+
+(define set?
+  (lambda (lat)
+    (cond
+      ((null? lat) #t)
+      ((member? (car lat) (cdr lat)) #f)
+      (else (set? (cdr lat))))))
+
+(define member?
+  (lambda (a lat)
+    (cond
+      ((null? lat) #f)
+      (else (or (equal? (car lat) a) (member? a (cdr lat)))))))
+
+(define makeset
+  (lambda (lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else (cons (car lat) (makeset (multirember (car lat) (cdr lat))))))))
+
+(define multirember
+  (lambda (a lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else (cond
+              ((equal? (car lat) a) (multirember a (cdr lat)))
+              (else (cons (car lat) (multirember a (cdr lat)))))))))
+
+(define subset?
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) #t)
+      (else (and (member? (car set1) set2) (subset? (cdr set1) set2))))))
+
+(define eqset?
+  (lambda (set1 set2)
+    (and (subset? set1 set2) (subset? set2 set1))))
+
+(define intersect?
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) #f)
+      (else (or (member? (car set1) set2) (intersect? (cdr set1) set2))))))
+
+(define intersect
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) (quote ()))
+      ((member? (car set1) set2) (cons (car set1) (intersect (cdr set1) set2)))
+      (else (intersect (cdr set1) set2)))))
+
+(define union
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) set2)
+      ((member? (car set1) set2) (union (cdr set1) set2))
+      (else (cons (car set1) (union (cdr set1) set2))))))
+
+(define difference
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) (quote()))
+      ((member? (car set1) set2) (difference (cdr set1) set2))
+      (else (cons (car set1) (difference (cdr set1) set2))))))
+
+(define intersectall
+  (lambda (l-set)
+    (cond
+      ((null? (cdr l-set)) (car l-set))
+      (else (intersect (car l-set) (intersectall (cdr l-set)))))))
+
+(define a-pair?
+  (lambda (x)
+    (cond
+      ((atom? x) #f)
+      ((null? x) #f)
+      ((null? (cdr x)) #f)
+      (else (null? (cdr (cdr x)))))))
+
+(define first
+  (lambda (p)
+    (car p)))
+
+(define second
+  (lambda (p)
+    (car (cdr p))))
+
+(define build
+  (lambda (s1 s2)
+    (cons s1 (cons s2 (quote ())))))
+
+(define third
+  (lambda (l)
+    (car (cdr (cdr l)))))
+
+(define fun?
+  (lambda (rel)
+    (set? (firsts rel))))
+
+(define revpair
+  (lambda (pair)
+    (build (second pair) (first pair))))
+
+(define revrel
+  (lambda (rel)
+    (cond
+      ((null? rel) (quote ()))
+      (else (cons (revpair (car rel)) (revrel (cdr rel)))))))
+
+(define fullfun?
+  (lambda (fun)
+    (set? (seconds fun))))
+
+(define seconds
+  (lambda (l)
+    (cond
+      ((null? l) (quote ()))
+      (else (cons (car (cdr (car l))) (seconds (cdr l)))))))
+
+(define one-to-one?
+  (lambda (fun)
+    (fun? (revrel fun))))
